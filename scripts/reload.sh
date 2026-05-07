0e268f23c18ce87881aafb11294b01e2771a28e7
Maulusck <64360319+maulusck@users.noreply.github.com>
2026-05-07 08:57:09 +0000

    bugfix to the important fix

--- scripts/reload.sh
+++ scripts/reload.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-cd $(dirname ${0})
+cd "$(dirname ${0})/../"
 podman build -t websrv .
 podman image prune -f
 exec podman run --name websrv --replace -d --rm -v $(pwd)/webroot:/var/www/html:Z -p 8080:80 websrv
